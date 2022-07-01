2d5fdf8ac00d2f8e1de7ba4bc508db5ffadf61b6
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-02 02:09:42 +0530

    Added: Welcome message added

---
+++ src/com/bridgelabz/StacksAndQueues.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class StacksAndQueues {
+    public static void main(String args[]){
+        System.out.println("Welcome to Stacks and Queues Program.");
+    }
+}
