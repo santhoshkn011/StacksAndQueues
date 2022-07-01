39a794d34cf7653e6a26dad4c67594e4beae4b22
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-02 03:14:54 +0530

    Added: Queue program created

--- src/com/bridgelabz/MyQueue.java
+++ src/com/bridgelabz/MyQueue.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 public class MyQueue {
     int data;
     MyQueue next;
-
     // constructor to create a new linked list node
     public MyQueue(int data) {
         this.data = data;
